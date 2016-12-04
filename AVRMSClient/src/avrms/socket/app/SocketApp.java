812b370fcbb2be45074fb9876483434a6afd7e98
Jae Wook Lim <jaeseung17@MacBook-Pro.local>
2016-12-04 23:14:48 +0900

    able to get Battery and state information

--- AVRMSClient/src/avrms/socket/app/SocketApp.java
+++ AVRMSClient/src/avrms/socket/app/SocketApp.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,56 +18,95 @@ import org.json.simple.*;
 
 import avrms.socket.WebsocketClientEndpoint;
 import avrms.socket.bean.NavSatFix;
+import avrms.socket.message.MAVROSMessageParser;
 
 public class SocketApp {
+	private static WebSocketClient wsc;
+	private static MAVROSMessageParser msgparser = new MAVROSMessageParser();
 	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 		/**
-		 * args[0] : address (#ip#:#port#/#applicationName#)- ex) 127.0.0.1:8080/avrms
-		 * args[1] : vehicle identification name 
+		 * args[0] : node name
+		 * args[1] : address (#ip#:#port#/#applicationName#)- ex) 127.0.0.1:8080/avrms
+		 * args[2] : vehicle identification name 
 		 */
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("args: "+args[i]);
-		}
-		
-		WebSocketClient wsc = new WebSocketClient(new URI("ws://"+args[0]+"/realtime/echo.do"),new Draft_17()) {
+		String node_name = args[0];
+		wsc = new WebSocketClient(new URI("ws://"+args[1]+"/realtime/echo.do"),new Draft_17()) {
 			Boolean approved = false;
 			@Override
 			public void onOpen(ServerHandshake arg0) {
 				// TODO Auto-generated method stub
 				System.out.println("opened");
 				Process proc;
+				Process proc_test, proc_gps,proc_state,proc_local_pos,proc_battery;
+
 				/**
 				 * 연결 허가
 				 */
-				this.send("APPROVAL: "+args[1]);
+				this.send("APPROVAL: "+args[2]);
+
+				JSONObject json = new JSONObject();
+				json.put("id", args[2]);
+
+				Thread gps_mon = new Thread(new Runnable() {
+					public void run() {
+						try {
+							Process proc = Runtime.getRuntime().exec("rostopic echo /"+node_name+"/global_position/raw/fix");
+							_readOutPut(proc, "gps");
+
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				gps_mon.start();
+
+				Thread state_mon = new Thread(new Runnable() {
+					public void run() {
+						try {
+							Process proc = Runtime.getRuntime().exec("rostopic echo /"+node_name+"/state");
+							_readOutPut(proc, "state");
+
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				state_mon.start();
+
+				Thread battery_mon = new Thread(new Runnable() {
+					public void run() {
+						try {
+							Process proc = Runtime.getRuntime().exec("rostopic echo /"+node_name+"/battery");
+							_readOutPut(proc, "battery");
+
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				battery_mon.start();
 				
 				try {
-					JSONObject json = new JSONObject();
-					json.put("id", args[1]);
-					this.send("");
-					proc = Runtime.getRuntime().exec("rostopic echo /mavros/global_position/raw/fix");
-//					proc = Runtime.getRuntime().exec("ping google.com");
-
-					BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-					String line = null;
-					while((line = br.readLine())!=null){
-//						System.out.println(line);
-//						this.send(line);
-						String msg_to_send = MessageParsing(line);
-
-						if(msg_to_send!=null){
-							this.send(msg_to_send);
-						}
-						
-					}
-				
-				} catch (IOException e) {
+					gps_mon.join();
+				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 			@Override
 			public void onMessage(String arg0) {
 				// TODO Auto-generated method stub
@@ -86,87 +127,27 @@ public class SocketApp {
 		};
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 			public void run() {
-				System.out.println("addShutdownHook");
+				System.out.println("end of program");
 				wsc.close();
 			}
 		}));
 		wsc.connect();
 		
-		
-
-		
-//		// open websocket
-//        final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("wss://real.okcoin.cn:10440/websocket/okcoinapi"));
-//        
-//        // add listener
-//        clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-//            public void handleMessage(String message) {
-//                System.out.println(message);
-//            }
-//        });
         
 	}
-	public static void readOutPut(Process proc) throws IOException, InterruptedException, URISyntaxException{
-		// Read the output
-		// open websocket
-		
-		
-		
-		Thread.sleep(10000);
-	}
-	public static String MessageParsing(String msg){
-		String result = null;
-		
-		String latitude_pattern = "latitude: (.)*";
-		String longitude_pattern = "longitude: (.)*";
-		String altitude_pattern = "altitude: (.)*";
-		
-		Matcher matcher = null;
-		NavSatFix position = null;
-		
-		JSONObject json = null;
-		if(msg.matches(latitude_pattern)){
-//			System.out.println("latitude detected//"+msg);
-			Pattern lat_pat = Pattern.compile("latitude: (.*)");
-			matcher = lat_pat.matcher(msg);
-			if(matcher.find()){
-//				System.out.println("found - "+matcher.group(1));
-				position = new NavSatFix();
-				// 			   position.setLatitude(matcher.group(1));
-				json = new JSONObject();
-				json.put("latitude", matcher.group(1));
-				
-				result = json.toJSONString();
-				
-			}
-		}else if(msg.matches(longitude_pattern)){
-//			System.out.println("longitude detected//"+msg);
-			Pattern long_pat = Pattern.compile("longitude: (.*)");
-			matcher = long_pat.matcher(msg);
-			if(matcher.find()){
-//				System.out.println("found - "+matcher.group(1));
-				// 			   position.setLongitude(matcher.group(1));
-				json = new JSONObject();
-				json.put("longitude", matcher.group(1));
-				
-				result = json.toJSONString();
-			}
-		}else if(msg.matches(altitude_pattern)){
-//			System.out.println("altitude detected//"+msg);
-			Pattern alt_pat = Pattern.compile("altitude: (.*)");
-			matcher = alt_pat.matcher(msg);
-			if(matcher.find()){
-//				System.out.println("found - "+matcher.group(1));
-				// 			   position.setAltitude(matcher.group(1));
-				json = new JSONObject();
-				json.put("altitude", matcher.group(1));
+	
+	public static void _readOutPut(Process proc, String type)throws IOException, InterruptedException{
+    	// Read the output
+		BufferedReader reader =  new BufferedReader(new InputStreamReader(proc.getInputStream()));
+		String line = "";
+		while((line = reader.readLine()) != null){
 
-				result = json.toJSONString();
+			String msg_to_send = msgparser.parseMessage(type, line);
+
+			if(msg_to_send!=null){
+				wsc.send(msg_to_send);
 			}
-		}else{
-//			System.out.println("not detected//"+msg);
 		}
-		
-		return result;
 	}
+	
 }
